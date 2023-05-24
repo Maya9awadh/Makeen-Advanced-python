efb43be23c22a571f4f527da57adad065cad6116
Maya9awadh <125116838+Maya9awadh@users.noreply.github.com>
2023-05-24 10:03:54 +0400

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,8 @@
+c_to_f= lambda x:(x*(9/5)+32)
+print("celiciu to fehrenheit: ",c_to_f(30),'\n')
+
+sum_2= lambda x: x*2
+print("3*2: ",sum_2(3),'\n')
+
+square= lambda y: y**2
+print("6^2: ", square(6),'\n')
